b761d7dab7d2d8ca40a9bcb7bbc48ca2d3e1f3c1
Sunpreet Sandhu <sunpreetsandhu2002@gmail.com>
2024-01-17 11:36:01 -0500

    Changed image update method from POST to PUT

--- face-recognition-backend/server.js
+++ face-recognition-backend/server.js
@@ -72,7 +72,7 @@ app.get("/profile/:id", (req, res) => {
     res.status(400).json("not found");
   }
 });
-app.post("/image", (req, res) => {
+app.put("/image", (req, res) => {
   const { id } = req.body;
   let found = false;
   database.users.forEach((user) => {
